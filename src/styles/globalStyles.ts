8e9356301bcecc48177b26eed35d152147a8451b
weibenfalk <thomas.weibenfalk@gmail.com>
2022-11-03 11:21:24 +0100

    fixed blurry images

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -11,6 +11,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     * {
+        image-rendering: pixelated;
         font-family: 'Munro', 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     }
 `;
