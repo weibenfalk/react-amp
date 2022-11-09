8a276b960a535fb505cb130640ad46ef80654188
weibenfalk <thomas.weibenfalk@gmail.com>
2022-11-09 14:22:58 +0100

    removed user-select globally

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     * {
+        user-select: none;
         image-rendering: pixelated;
         font-family: 'Munro', 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
         color: #00e800;
